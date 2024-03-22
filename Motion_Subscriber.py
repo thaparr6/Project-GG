e2226089c89b04eaa2388338a1773110f03ccee2
thaparr6 <159021061+thaparr6@users.noreply.github.com>
2024-03-22 08:49:29 +0000

    Add files via upload

---
+++ Motion_Subscriber.py
@@ -0,0 +1,26 @@
+import paho.mqtt.client as mqtt
+
+def main():
+    mqttBroker = "broker.hivemq.com"
+    client = mqtt.Client("Sanitizing System")
+    client.connect(mqttBroker,1883)
+    client.on_connect = on_connect
+    client.on_message = on_message
+
+def on_connect(client,userdata,flags,rc):
+    client.connected_flag = True
+    print("Connected with motion sensor")
+    client.subscribe("Motion")
+    
+def detection(Motion):
+    if Motion == True:
+        print("The Process will not continue")
+    elif Motion == False:
+        print("Process wil continue")
+
+def on_message(client,userdata,msg):
+    motion = msg.payload.decode()
+    detection(motion)
+
+if __name__ == "__main__":
+    main()
