7f1c39a784365077c3f2dbd8d1f1992796720aff
nowickam1 <162703651+nowickam1@users.noreply.github.com>
2024-03-31 23:32:19 +0100

    Update Weather_Publisher.py
    Changed the name of the weather function to main, called main() towards the end of the code to enable running of the function. Additional minor adjustments were made to allow code to be executed, removed unnecessary while True: loop at the end of the code that resulted in constant printing.

--- Weather_Publisher.py
+++ Weather_Publisher.py
@@ -6,27 +6,31 @@ import json
 API_key = "19b21c8ed496b716ea542ad051cfb170"
 mqttBroker = "broker.hivemq.com"
 client = mqtt.Client("Weather System") #MQTT client initialisation
-client.on_connect = on_connect
 
 def on_connect(client, userdata, flags, rc):
     client.connect(mqttBroker, 1883)
     client.connected_flag = True
     print("Connected with weather system")
 
-def get_weather_forecast():
-    url = f"http://api.openweathermap.org/data/2.5/forecast?lat=52.41&lon=-1.51&appid={API_key}" #the coordinates for Coventry are (52.41, -1.51) and these are required for the API call
+def main():
+    url = f"http://api.openweathermap.org/data/2.5/forecast?lat=52.41&lon=-1.51&cnt=1&appid={API_key}" #the coordinates for Coventry are (52.41, -1.51) and cnt is used to limit the number of results returned
     response = requests.get(url)
     data = response.json()
     for forecast in data["list"]: #the rain category is listed under the 'list' variable in the API response
         if 'rain' not in data or '3h' not in data['rain'] or float(data['rain']['3h']) == 0:
-            return True
-    return False
+            return 1
+    return 0
+    print(data)
 
-while True:
-    if get_weather_forecast() == True: 
-        client.publish("cleaning/trigger", 1) #the conditions indicate no rain in the next three hours, so a 1 is published
-    else:
-        client.publish("cleaning/trigger", 0) #the conditions indicating it is or will rain have been set off, so a 0 is published
-        time.sleep(3600 * 3) #checks again in three hours time
+client.on_connect = on_connect
+
+main()
+
+
+if main() == 1: 
+    client.publish("cleaning/trigger", 1) #the conditions indicate no rain in the next three hours, so a 1 is published
+else:
+    client.publish("cleaning/trigger", 0) #the conditions indicating it is or will rain have been set off, so a 0 is published
+    time.sleep(3600 * 3) #checks again in three hours time
 
 client.loop_forever() #keeps running the loop until the program ends
