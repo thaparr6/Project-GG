341a6dfac4fbd0354752f85e832fa2c98298c6dc
mihirchauhan0310 <159139592+mihirchauhan0310@users.noreply.github.com>
2024-04-01 01:41:32 +0100

    Weather_Subscriber.py

--- Weather_Subscriber.py
+++ Weather_Subscriber.py
@@ -1,1 +1,49 @@
+import os
+import time
+import ssl
+import json
+import paho.mqtt.client as mqtt
 
+# Set the OpenWeather API key and city ID as environment variables
+OPENWEATHER_API_KEY = os.environ.get('OPENWEATHER_API_KEY')
+OPENWEATHER_CITY_ID = os.environ.get('OPENWEATHER_CITY_ID', '4236955')
+
+# Set the MQTT broker and topic
+MQTT_BROKER = 'broker.hivemq.com'
+MQTT_PORT = 1883
+MQTT_TOPIC = 'weather'
+
+# Set the MQTT client ID and message
+CLIENT_ID = f'openweather_{random.randint(0, 1000)}'
+MESSAGE = f'Weather in {OPENWEATHER_CITY_ID}:\n'
+
+def on_connect(client, userdata, flags, rc):
+    if rc == 0:
+        print(f'Connected to {MQTT_BROKER} with result code {rc}')
+        client.subscribe(MQTT_TOPIC)
+    else:
+        print(f'Failed to connect with result code {rc}')
+
+def on_message(client, userdata, message):
+    global MESSAGE
+    if message.topic == MQTT_TOPIC:
+        data = json.loads(message.payload)
+        if 'weather' in data:
+            weather = data['weather'][0]['description']
+            temp = data['main']['temp']
+            humidity = data['main']['humidity']
+            MESSAGE += f'- Weather: {weather}\n'
+            MESSAGE += f'- Temperature: {temp}K\n'
+            MESSAGE += f'- Humidity: {humidity}%\n'
+            print(MESSAGE)
+
+def main():
+    client = mqtt.Client(CLIENT_ID)
+    client.on_connect = on_connect
+    client.on_message = on_message
+    client.tls_set(ca_certs=None, certfile=None, keyfile=None, tls_version=ssl.PROTOCOL_TLS)
+    client.connect(MQTT_BROKER, MQTT_PORT)
+    client.loop_forever()
+
+if __name__ == '__main__':
+    main()
