e5d4db297cc91819f9aa1ce9b96e67acd8e7dec2
nowickam1 <162703651+nowickam1@users.noreply.github.com>
2024-04-01 17:16:18 +0100

    Update Weather_Publisher.py
    added time module to allow time.sleep()
    removed unnecessary client.loop_forever()
    adjusted main() function as python couldn't read API response
    added print statements at the bottom to indicate code working

--- Weather_Publisher.py
+++ Weather_Publisher.py
@@ -1,6 +1,7 @@
 import paho.mqtt.client as mqtt
 import requests
 import json
+import time
 
 #The API key was generated from the OpenWeatherMap website
 API_key = "2837c7dc12f0d19128fc0758d1f52efd"
@@ -16,11 +17,13 @@ def main():
     url = f"http://api.openweathermap.org/data/2.5/forecast?lat=52.41&lon=-1.51&cnt=1&appid={API_key}" #the coordinates for Coventry are (52.41, -1.51) and cnt is used to limit the number of results returned
     response = requests.get(url)
     data = response.json()
-    for forecast in data["list"]: #the rain category is listed under the 'list' variable in the API response
-        if 'rain' not in data or '3h' not in data['rain'] or float(data['rain']['3h']) == 0:
-            return 1
+    if "list" not in data or "rain" not in data["list"][0]:
+        return 1
+
+    if float(data["list"][0]["rain"]["3h"]) == 0:
+        return 1
+        
     return 0
-    print(data)
 
 client.on_connect = on_connect
 
@@ -29,8 +32,9 @@ main()
 
 if main() == 1: 
     client.publish("cleaning/trigger", 1) #the conditions indicate no rain in the next three hours, so a 1 is published
-else:
+    print("no rain")
+elif main() == 0:
     client.publish("cleaning/trigger", 0) #the conditions indicating it is or will rain have been set off, so a 0 is published
-    time.sleep(3600 * 3) #checks again in three hours time
+    print("rain")
+    time.sleep(3600 * 3) #waits for three hours 
 
-client.loop_forever() #keeps running the loop until the program ends
