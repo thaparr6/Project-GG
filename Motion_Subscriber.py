68966ac86a60e70dc831fcb00c6387b36198d932
thaparr6 <159021061+thaparr6@users.noreply.github.com>
2024-04-02 15:26:40 +0100

    Update Motion_Subscriber.py

--- Motion_Subscriber.py
+++ Motion_Subscriber.py
@@ -3,12 +3,12 @@ import paho.mqtt.client as mqtt
 def main():
     mqttBroker = "broker.hivemq.com"
     client = mqtt.Client("Sanitizing System")
-    client.connect(mqttBroker,1883)
     client.on_connect = on_connect
     client.on_message = on_message
 
 def on_connect(client,userdata,flags,rc):
     client.connected_flag = True
+    client.connect(mqttBroker,1883)
     print("Connected with motion sensor")
     client.subscribe("Motion")
     
@@ -22,5 +22,5 @@ def on_message(client,userdata,msg):
     motion = msg.payload.decode()
     detection(motion)
 
-if __name__ == "__main__":
-    main()
+main()
+
